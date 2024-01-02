ed7fab036e4fe48ff348f99a225bea5e97d54a35
Gorkem Kacar <gorkemkacar12@gmail.com>
2024-01-02 11:19:01 +0300

    test added for models post , users and profile

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+import unittest
+from .models import Post
+from django.contrib.auth.models import User
+from django.core import exceptions as exception
 
-# Create your tests here.
+
+class PostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username = 'testuser' , email = 'aaa@gmail.com')
+        self.user.set_password('aab12345')
+        self.user.save()
+        self.post =  Post.objects.create(title = 'thistestpost' , content = 'test post content' , author = self.user)
+        self.post.save()
+    def test_post_created(self):           
+        self.assertEqual(self.post.title  , 'thistestpost')
+        self.assertEqual(self.post.content , 'test post content')
+        self.assertEqual(self.post.author , self.user)
+
+    def test_post_updated(self):
+        self.post.content = 'test post content updated'
+        self.post.save()
+        self.assertEqual(self.post.content  , 'test post content updated')
+    
+    def test_post_deleted(self):
+        self.post.delete()
+        with self.assertRaises(exception.ObjectDoesNotExist):
+            Post.objects.get(title = 'thistestpost' , author = self.user)
+
+
+        
+        
+
+
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
+import unittest  
 
-# Create your tests here.
+
+class UserTest(TestCase):
+    def setUp(self):
+        user =  User.objects.create(username = 'cccc' , email = 'abc@gmail.com')
+        user.set_password('abc12345')
+        user.save()
+    def test_user_created(self):
+        user = User.objects.get(username = 'cccc')
+        profile = Profile.objects.get(user = user)
+        self.assertEqual(user.email  , 'abc@gmail.com')
+        self.assertEqual(user.check_password('abc12345') , True)
+        self.assertEqual(profile.user , user)
+        self.assertEqual(profile.image.name , 'default.png')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
