c3ee7bfc914d458d71976f0fbc26aef3290e2eaf
Gorkem Kacar <gorkemkacar12@gmail.com>
2024-01-02 11:49:36 +0300

    in users.tests testing profile is now seperated from user_created

--- users/tests.py
+++ users/tests.py
@@ -6,17 +6,16 @@ import unittest
 
 class UserTest(TestCase):
     def setUp(self):
-        user =  User.objects.create(username = 'cccc' , email = 'abc@gmail.com')
-        user.set_password('abc12345')
-        user.save()
+        self.user =  User.objects.create(username = 'cccc' , email = 'abc@gmail.com')
+        self.user.set_password('abc12345')
+        self.user.save()
     def test_user_created(self):
-        user = User.objects.get(username = 'cccc')
-        profile = Profile.objects.get(user = user)
-        self.assertEqual(user.email  , 'abc@gmail.com')
-        self.assertEqual(user.check_password('abc12345') , True)
-        self.assertEqual(profile.user , user)
-        self.assertEqual(profile.image.name , 'default.png')
-
+        self.assertEqual(self.user.email  , 'abc@gmail.com')
+        self.assertEqual(self.user.check_password('abc12345') , True)       
+    def test_profile_created(self):
+        self.profile = Profile.objects.get(user = self.user)
+        self.assertEqual(self.profile.user , self.user)
+        self.assertEqual(self.profile.image.name , 'default.png')
 
 
 if __name__ == '__main__':
