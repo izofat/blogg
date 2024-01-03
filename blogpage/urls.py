256f63a55b645c06a83b9eeb591b64926bb28236
Gorkem Kacar <gorkemkacar12@gmail.com>
2024-01-03 23:05:39 +0300

    code format

--- blogpage/urls.py
+++ blogpage/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 from django.contrib.auth.views import LoginView
 
 from users import views as usersView
@@ -8,15 +8,21 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('' , include('blog.urls')),
-    path('login/' , LoginView.as_view(template_name= 'users/login.html' , extra_context={'title' : 'Login'}) , name='login' ),
-    path('logout_user/' , usersView.logout_user , name='logout_user'),
-    path('logout_view/' , usersView.logout_view , name='logout_view') ,
-    path('register/' , usersView.register_user , name='register'),
-    path('profile/' , usersView.profile  , name='profile'),
-    path('resetpassword/' , usersView.reset_password , name='reset-password')
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="users/login.html", extra_context={"title": "Login"}
+        ),
+        name="login",
+    ),
+    path("logout_user/", usersView.logout_user, name="logout_user"),
+    path("logout_view/", usersView.logout_view, name="logout_view"),
+    path("register/", usersView.register_user, name="register"),
+    path("profile/", usersView.profile, name="profile"),
+    path("resetpassword/", usersView.reset_password, name="reset-password"),
 ]
 
 if settings.DEBUG:
-    urlpatterns +=  static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
