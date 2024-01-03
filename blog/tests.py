d490409ee4fb9a9d506c6db6d3807164369b9dce
Gorkem Kacar <gorkemkacar12@gmail.com>
2024-01-03 12:51:02 +0300

    more tests added to blog and user apps

--- blog/tests.py
+++ blog/tests.py
@@ -1,9 +1,10 @@
-from django.test import TestCase
-import unittest
+from django.test import TestCase , Client
+from django.urls import reverse , resolve
+from . import views 
 from .models import Post
 from django.contrib.auth.models import User
 from django.core import exceptions as exception
-
+from django.contrib.auth.models import User
 
 class PostTest(TestCase):
     def setUp(self):
@@ -30,3 +31,82 @@ class PostTest(TestCase):
         self.post.delete()
         with self.assertRaises(exception.ObjectDoesNotExist):
             Post.objects.get(title="thistestpost", author=self.user)
+
+class URLTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username = 'test' , email='a@gmail.com')
+        self.user.set_password('aaa123123')
+        self.post = Post.objects.create(title = 'aa' , content = 'aa' , author = self.user)
+
+    def test_basic_urls_resolved_without_params(self):
+        url_names = ['blog-home'  , 'post-create'  , 'posts-latest' , 'announcements']
+        view_classes = ['PostListView'  , 'PostCreateView'  , 'LatestPostsView' , 'AnnouncementsView']
+        for i in range(len(url_names)):
+            url_name = url_names[i]
+            view_class = view_classes[i]
+            url =reverse(url_name)
+            resolved_view = resolve(url).func.view_class
+            expected_view = getattr(views ,view_class , None)
+            with self.subTest(url_name = url_name):
+                self.assertEqual(resolved_view , expected_view)
+    def test_urls_resolved_with_params(self):
+        url_names = ['post-detail'  , 'post-update'  , 'post-delete' , 'allposts-user']
+        view_classes = ['PostDetailView'  , 'PostUpdateView'  , 'PostDeleteView' , 'UserPostListView']
+        for i in range(len(url_names)):
+            url_name = url_names[i]
+            view_class = view_classes[i]
+
+            if url_name == 'allposts-user':
+                url = reverse(url_name , kwargs={'username' : self.user.username} )
+            else:
+                url =reverse(url_name , kwargs={'pk' : self.post.id})
+            
+            resolved_view = resolve(url).func.view_class
+            expected_view = getattr(views ,view_class , None)
+            with self.subTest(url_name = url_name):
+                self.assertEqual(resolved_view , expected_view)
+
+
+class ViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create(username = 'test' , email='a@gmail.com')
+        self.user.set_password('aaa123123')
+        self.post = Post.objects.create(title = 'aa' , content = 'aa' , author = self.user)
+        self.client.force_login(self.user)
+    def test_post_view_GET(self):
+        url_names = ['blog-home' , 'posts-latest' , 'announcements' , 'post-detail' ,'allposts-user']
+        template_names = ['home.html' , 'latest_posts.html' , 'announcements.html' , 'post_detail.html' , 'allpostuser.html']
+        for i in range(len(url_names)):
+            url_name = url_names[i]
+            template_name = template_names[i]
+            if url_name == 'post-detail':
+                url = reverse(url_name , kwargs={'pk' : self.post.id})
+            elif url_name == 'allposts-user':
+                url = reverse(url_name , kwargs={'username' : self.user.username})
+            else:
+                url = reverse(url_name)
+            
+            response = self.client.get(url)
+            self.assertEqual(response.status_code , 200)
+            self.assertTemplateUsed(response , 'blog/' + template_name )
+
+    def test_post_view_POST_create(self):
+        url_create = reverse('post-create')
+        response_create =  self.client.post(url_create , {'title' : 'aaa' , 'content' : 'bbb' , 'author' : self.user})
+        self.assertEqual(response_create.status_code , 302)
+    def test_post_view_POST_update(self):
+        post1 = Post.objects.create(title = 'aaa' , content = 'bbb' , author = self.user)
+        url_update = reverse('post-update' , kwargs={'pk' : post1.id})
+        update_data = {'title' : 'kkk' ,  'content' : 'ppp'}
+        response_update = self.client.post(url_update  , update_data)
+        self.assertEqual(response_update.status_code , 302)
+    def test_post_view_DELETE_delete(self):
+        url_delete = reverse('post-delete' , kwargs={'pk' : self.post.id})
+        response_get = self.client.get(url_delete)
+        self.assertEqual(response_get.status_code , 302)
+
+        response_delete = self.client.delete(url_delete)
+        self.assertEqual(response_delete.status_code , 302)
+        
+            
--- users/tests.py
+++ users/tests.py
@@ -1,9 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase , Client
 from .models import Profile
 from django.contrib.auth.models import User
-import unittest  
-
-
+from django.urls import reverse , resolve
+from . import views
+from django.contrib.auth.views import LoginView
+from django.core.files.uploadedfile import SimpleUploadedFile
+import os
+from django.conf import settings
 class UserTest(TestCase):
     def setUp(self):
         self.user =  User.objects.create(username = 'cccc' , email = 'abc@gmail.com' , first_name='aaa' , last_name ='kkk')
@@ -18,6 +21,85 @@ class UserTest(TestCase):
         self.assertEqual(profile.user , self.user)
         self.assertEqual(profile.image.name , 'default.png')
 
+class URLTest(TestCase):
+    def test_urls_resolved(self):
+        url_names = [ 'login' ,'logout_user' , 'logout_view'  , 'register' , 'profile' , 'reset-password']
+        view_names = [ 'LoginView', 'logout_user' , 'logout_view' , 'register_user' ,'profile' ,'reset_password' ]
+        for i in range(len(url_names)):
+            url_name = url_names[i]
+            view_name = view_names[i]
 
-if __name__ == '__main__':
-    unittest.main()
+            url = reverse(url_name)
+            if url_name == 'login':
+                resolved_view = resolve(url).func.view_class
+                expected_view = LoginView
+            else:
+                resolved_view = resolve(url).func
+                expected_view = getattr(views , view_name , None)
+            
+            with self.subTest(url_name = url_name):
+                self.assertEqual( resolved_view ,expected_view)
+
+class ViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user =  User.objects.create(username = 'testusernametestusername' , email = 'abc@gmail.com')
+        self.user.set_password('abc12345')
+        self.user.save()
+    def test_view_register(self):
+        url = reverse('register')
+        response_get = self.client.get(url)
+        self.assertEqual(response_get.status_code , 200)
+        username = 'kkkkkkk'
+        response_post = self.client.post(url , {'username' : username  , 'email' : 'abc@gmail.com'  , 'password1' : 'abcabc12' , 'password2' : 'abcabc12'})
+        self.assertTrue(User.objects.filter(username = username).exists())        
+        self.assertEqual(response_post.status_code , 302)
+        self.assertRedirects(response_post , reverse('login'))
+
+    def test_view_login(self):
+        url = reverse('login')
+        response_get = self.client.get(url)
+        self.assertEqual(response_get.status_code , 200)
+        response_post = self.client.post(url , {'username' : 'testusernametestusername' , 'password' : 'abc12345'})
+
+        self.assertEqual(response_post.status_code , 302)
+        self.assertRedirects(response_post , reverse('blog-home'))
+
+    def test_view_logout(self):
+        
+        self.client.force_login(self.user)
+        url = reverse('logout_user')
+        response_get = self.client.get(url)
+        self.assertEqual(response_get.status_code ,302)
+    
+    def test_view_profile(self):
+        self.client.force_login(self.user)
+
+        url = reverse('profile')
+        response_get = self.client.get(url)
+        self.assertEqual(response_get.status_code , 200)
+        file_path = os.path.join(settings.MEDIA_ROOT , 'test.png')
+        with open(file_path , 'rb') as picture :
+            image = SimpleUploadedFile('test.png' , picture.read() , content_type='image/png')
+        response_post = self.client.post(url , {'username' : 'testusernameaaaaa','email' : 'aaa1234@gmail.com'  , 'first_name' : 'ka' , 'last_name':'aaa', 'image' : image })
+        self.assertEqual(response_post.status_code , 302)
+        profile = Profile.objects.get(user = self.user)
+        self.assertEqual(profile.user.username , 'testusernameaaaaa')
+        self.assertEqual(profile.user.email , 'aaa1234@gmail.com')
+        self.assertEqual(profile.user.first_name , 'ka')
+        self.assertEqual(profile.user.last_name , 'aaa')
+        self.assertTrue('test' in profile.image.name )
+
+    def test_view_reset_password(self):
+        ##! login for not to redirect login
+        self.client.force_login(self.user)
+        url = reverse('reset-password')
+        response_get = self.client.get(url)
+
+        self.assertEqual(response_get.status_code , 200)    
+
+        response_post = self.client.post(url , {'old_password' : 'abc12345' , 'new_password1' : 'abc12345678910' , 'new_password2' : 'abc12345678910' })
+        updated_user = User.objects.get(id = self.user.id)
+        self.assertTrue(updated_user.check_password('abc12345678910'))
+        self.assertEqual(response_post.status_code , 302)
+        self.assertRedirects(response_post , reverse('blog-home'))
